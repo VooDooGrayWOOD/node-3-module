4c33dc8907e37343fcf916a007442cd89866cfe1
Sergey <89291563+VooDooGrayWOOD@users.noreply.github.com>
2022-07-12 18:06:56 +0700

    delete object & show ID in list of notes

---
+++ notes.controller.js
@@ -0,0 +1,51 @@
+const fs = require('fs/promises')
+const path = require('path')
+
+const chalk = require('chalk')
+
+const notesPath = path.join(__dirname, 'ds.json')
+console.log(notesPath)
+
+
+async function addNote(title) {
+	const notes = await getNotes()
+	const note = {
+		title,
+		id: Date.now().toString()
+	}
+	notes.push(note)
+
+	await fs.writeFile(notesPath, JSON.stringify(notes))
+	console.log(chalk.bgGreen('Note was added!'))
+}
+
+async function getNotes() {
+	const notes = await fs.readFile(notesPath, {encoding: 'utf-8'})
+	return Array.isArray(JSON.parse(notes)) ? JSON.parse(notes) : []
+}
+
+async function printNotes() {
+	const notes = await getNotes()
+
+	console.log(chalk.bgBlue('Here is the list of notes:'))
+	notes.forEach(note => {
+		console.log(chalk.blue('Id:', note.id, 'Title:', note.title))
+	})
+
+}
+
+async function removeNotes(id) {
+	const notes = await getNotes(id)
+	notes.forEach((val, key)=> {
+		if(val.id == id) {
+			delete notes[key]
+			const newNotes = notes.filter(el => el !== null)
+			fs.writeFile(notesPath, JSON.stringify(newNotes))
+		}
+	})
+	console.log(chalk.red('We remove notes by id:', id))
+}
+
+module.exports = {
+	addNote, printNotes, removeNotes
+}
